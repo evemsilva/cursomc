9e24ccbc8cdacdbb1c08b3b87a9d7f4de80ec2ba
Everton Monte da Silva <everton0189@gmail.com>
2018-03-25 15:04:49 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.nelioalves.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+		 Cliente obj = clienteService.buscar(id);
+		 return ResponseEntity.ok(obj);
+	}
+
+}
